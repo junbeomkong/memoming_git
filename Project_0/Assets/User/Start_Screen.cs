d25627892e8f705f3974cdf15e4841360cab242d
hajihye <hjie0314@gmail.com>
2020-10-07 01:19:17 +0900

    2020-10-07 프로토타입

---
+++ Project_0/Assets/User/Start_Screen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Screen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(1280, 720, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project_0/Assets/User/button_1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class button_1 : MonoBehaviour
+{
+    GameObject Magnifying_Panel = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Magnifying_Panel = GameObject.Find("c").transform.FindChild("Magnifying_Panel").gameObject;
+    }
+
+
+        public void ChangeFirstScene()//창업 시작하기 버튼
+    {
+        SceneManager.LoadScene("01.Sectors");
+    }
+
+    public void Magnifying_Glass()//돋보기 클릭
+    {
+       Magnifying_Panel.SetActive(true);
+    }
+    
+    public void Magnifying_Glass2()//돋보기 - 확인 클릭
+    {
+       Magnifying_Panel.SetActive(false);
+    }
+    public void Convenience()//편의점 선택
+    {
+        //편의점 클릭시 변수 추가
+        SceneManager.LoadScene("02.capital");
+    }
+    public void cafe()//카페 선택
+    {
+        //카페 클릭시 변수 추가
+        SceneManager.LoadScene("02.capital");
+    }
+}
