6b0b7f61fbfbac60da9bd675f576098361a36d28
hajihye <hjie0314@gmail.com>
2020-10-12 00:15:34 +0900

    2020-10-12

---
+++ Project_0/Assets/Scene_factor/2-1(편의점)/2-3(편의점 규모)/button_3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class button_3 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+   
+    public void capital()//자본금 수정
+    {
+        SceneManager.LoadScene("2-2.capital");
+    }
+    public void next()//다음 단계
+    {
+        //카페 클릭시 변수 추가
+        SceneManager.LoadScene("2-4");
+    }
+}
